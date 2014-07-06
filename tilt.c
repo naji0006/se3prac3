533b8b7a2e366c6131cc8d6c2f9e9705dda5d3fb
naji0006 <naji0006@flinders.edu.au>
2014-07-06 22:16:39 +0930

    Added shift_left function for shifting values to left.

--- tilt.c
+++ tilt.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "2048.h"
 
+int shift_left(int *a) {
+    int i;
+    for (i=2; i>0; i--) {
+        int j;
+        for (j=3; j>=1; j--) {
+            if (a[j] != 0 && a[j-1] == 0) {
+                a[j-1] = a[j];
+                a[j] = 0;
+            }
+        }
+    }
+    return 0;
+}
+
 int tilt_line_left(int length,int *line)
 {
   // make sure vector length is sensible
