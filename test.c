341d8f40029b5e95c690c34df0664031eb5bfb10
naji0006 <naji0006@flinders.edu.au>
2014-07-06 22:12:03 +0930

    Adding test for left shift functionality.

--- test.c
+++ test.c
@@ -49,6 +49,21 @@ int test_tilt_left()
   return e;
 }
 
+int ttl_vector_shift(int i1, int i2, int i3, int i4, char *msg,
+        int o1, int o2, int o3, int o4) {
+    return line_vector_test(i1, i2, i3, i4, msg, o1, o2, o3, o4, shift_left);
+}
+
+int test_shift_left(){
+  int e=0;
+  e|=ttl_vector_shift(0,0,0,0,"Empty list is empty after shift",0,0,0,0);
+  e|=ttl_vector_shift(1,0,0,0,"Value on left stays on left after shift",1,0,0,0);
+  e|=ttl_vector_shift(0,0,0,1,"Value on right shifts to left edge after shift",1,0,0,0);
+  e|=ttl_vector_shift(0,1,0,0,"Value in middle shifts to left edge after shift",1,0,0,0);
+  e|=ttl_vector_shift(2,0,2,1,"All values shift left",2,2,1,0);
+  return e;
+}
+
 int main(int argc,char **argv)
 {
   int e=0;
