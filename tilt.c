b8afb7c8a16d2897a128a21a4c9cd6dc6c7b1527
naji0006 <naji0006@flinders.edu.au>
2014-07-06 22:17:58 +0930

    Completed method tilt_line_left function with combining functionality.

--- tilt.c
+++ tilt.c
@@ -15,14 +15,22 @@ int shift_left(int *a) {
     return 0;
 }
 
-int tilt_line_left(int length,int *line)
-{
-  // make sure vector length is sensible
-  if (length<1||length>255) return -1;
+int tilt_line_left(int length, int *line) {
+    // make sure vector length is sensible
+    if (length < 1 || length > 255) return -1;
 
-  // slide tiles to the left
-
-  // combine tiles as required
-
-  return 0;
+    // slide tiles to the left
+    shift_left(line);
+            
+    // combine tiles as required
+    int i;
+    for (i = 0; i < 3; i++) {
+        if (line[i] == line[i + 1]) {
+            line[i] *= line[i];
+            line[i + 1] = 0;
+        }
+    }
+    shift_left(line);
+    
+    return 0;
 }
