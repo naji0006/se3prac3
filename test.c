3e88585241479708bf51ca96a1c707484e69d8fc
naji0006 <naji0006@flinders.edu.au>
2014-07-07 12:58:59 +0930

    Added tile to board in test.

--- test.c
+++ test.c
@@ -254,6 +254,57 @@ int test_board_4_4() {
           );
 }
 
+int board_add_tile(int i1, int i2, int i3, int i4,
+        int i5, int i6, int i7, int i8,
+        int i9, int i10, int i11, int i12,
+        int i13, int i14, int i15, int i16,
+        char *msg, int willFail) {
+    
+    int **board = alloca(4 * sizeof (int*));
+    board[0] = alloca(sizeof (int)*4);
+    board[1] = alloca(sizeof (int)*4);
+    board[2] = alloca(sizeof (int)*4);
+    board[3] = alloca(sizeof (int)*4);
+
+    board[0][0] = i1;
+    board[1][0] = i2;
+    board[2][0] = i3,
+    board[3][0] = i4;
+    board[0][1] = i5;
+    board[1][1] = i6;
+    board[2][1] = i7, 
+    board[3][1] = i8;
+    board[0][2] = i9;
+    board[1][2] = i10;
+    board[2][2] = i11, 
+    board[3][2] = i12;
+    board[0][3] = i13;
+    board[1][3] = i14;
+    board[2][3] = i15, 
+    board[3][3] = i16;
+
+    if (msg) printf("%s - ", msg);
+    else {
+        printf("Board operation on \n");
+        board_display(4, board);
+        printf("yields\n");
+        board_display(4, board);
+    }
+    fflush(stdout);
+    int success = board_spawn_tile(4, board);
+    if ((success == 0 && willFail == 0) || (success == 1 && willFail == 1)) {
+        printf("FAILED: {{%d,%d,%d,%d},{%d,%d,%d,%d},{%d,%d,%d,%d},{%d,%d,%d,%d}} became {{%d,%d,%d,%d},{%d,%d,%d,%d},{%d,%d,%d,%d},{%d,%d,%d,%d}}, tile added:%d\n\n",
+                i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16,
+                board[0][0], board[1][0], board[2][0], board[3][0],
+                board[0][1], board[1][1], board[2][1], board[3][1],
+                board[0][2], board[1][2], board[2][2], board[3][2],
+                board[0][3], board[1][3], board[2][3], board[3][3], success);
+        return -1;
+    }
+    printf("PASSED.\n");
+    return 0;
+}
+
 //int ttr_vector_shift(int i1, int i2, int i3, int i4, char *msg,
 //        int o1, int o2, int o3, int o4) {
 //    return line_vector_test(i1, i2, i3, i4, msg, o1, o2, o3, o4, shift_right);
